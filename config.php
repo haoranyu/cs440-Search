1fb2236f3cd130a64cbea7d8ca7bfa2a2f13e869
haoranyu <haoran_yu@hotmail.com>
2013-09-23 20:16:43 -0500

    add interface

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 	header('Content-Type: text/plain');
-	$maze = file_get_contents('test.lay');
-	//$maze = file_get_contents('http://www.cs.illinois.edu/~slazebni/fall13/assignment1/openMaze.lay');
-	//exit($maze);
+	if(isset($_GET['map'])){
+		$maze = file_get_contents('http://www.cs.illinois.edu/~slazebni/fall13/assignment1/'.$_GET['map'].'Maze.lay');
+	}
+	else{
+		exit("no map specified");
+	}
 ?>

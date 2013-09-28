a9198b94a2e8ba1bb0835333c93c412f7361fb71
haoranyu <haoran_yu@hotmail.com>
2013-09-27 22:29:51 -0500

    Finish 1.2 part

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
 	header('Content-Type: text/plain');
 	if(isset($_GET['map'])){
-		$maze = file_get_contents('http://www.cs.illinois.edu/~slazebni/fall13/assignment1/'.$_GET['map'].'Maze.lay');
+		//$maze = file_get_contents('http://www.cs.illinois.edu/~slazebni/fall13/assignment1/'.$_GET['map'].'Maze.lay');
+		$maze = file_get_contents('test.lay');
 	}
 	else{
 		exit("no map specified");
--- function.ucs.php
+++ function.ucs.php
@@ -1,15 +1,20 @@
 <?php 
+function ucsH($x, $cost){
+	$newCost1 = pow(1/2,$x);
+	$newCost2 = pow(2,$x);
+	return  $newCost1 + $cost; //please change the return part to get different cost function
+}
 function ucs($maze, $start){
-	$pqueue = array(array($maze[$start['Y']][$start['X']],'x','x',"cost" => 0));
+	$pqueue = array(array($maze[$start['Y']][$start['X']],'x','x',"dis" => 0, "cost" => 0));
 	$maze[$start['Y']][$start['X']]["STAT"] = 1;
-	$maze = ucs_helper($maze, $pqueue);
+	$maze = ucs_helper($maze, $pqueue, 0, 1, 0);
 	return $maze;
 }
 function array_uppri($array){
 	$obj = 0;
 	$size = sizeof($array);
 	for($i = 0; $i < $size; $i++){
-		if($array[$i]["cost"] < $array[$obj]["cost"]){
+		if(($array[$i]["dis"]) < ($array[$obj]["dis"])){
 			$obj = $i;
 		}
 	}
@@ -19,13 +24,20 @@ function array_uppri($array){
 	return $array;
 }
 
-function ucs_helper($maze, $pqueue){
+function ucs_helper($maze, $pqueue, $counter, $frontier, $depth){
+	if($frontier < sizeof($pqueue)){
+		$frontier = sizeof($pqueue);
+	}
+	if(empty($pqueue)){
+		return -1;
+	}
 	$pqueue = array_uppri($pqueue);
 	$now = array_shift($pqueue);
-	
+	//echo $now[0]['X'].",".$now[0]['Y'].": ".($now['dis']).";".$now['cost']."\n";
+	$depth = max($depth,$now['cost']);
 	$maze[$now[0]['Y']][$now[0]['X']]["PREV"] = array($now[1],$now[2]);
 	if($maze[$now[0]['Y']][$now[0]['X']]["CONT"] == "."){
-		return $maze;
+		return array($maze,$now['cost'],$counter, $frontier, $depth);
 	}
 	else{
 		if($maze[$now[0]['Y']][$now[0]['X']+1]["STAT"] == 0){
@@ -33,7 +45,7 @@ function ucs_helper($maze, $pqueue){
 			array_push($pqueue, array(	$maze[$now[0]['Y']][$now[0]['X']+1],
 										$now[0]['X'],
 										$now[0]['Y'],
-										
+										"dis" => ucsH($now['dis'], $now[0]['X'] + 1), 
 										"cost" => ($now['cost'] + 1)
 									  ));
 		}
@@ -41,7 +53,8 @@ function ucs_helper($maze, $pqueue){
 			$maze[$now[0]['Y']+1][$now[0]['X']]["STAT"] = 1;
 			array_push($pqueue, array(	$maze[$now[0]['Y']+1][$now[0]['X']],
 										$now[0]['X'],
-										$now[0]['Y'], 
+										$now[0]['Y'],
+										"dis" => ucsH($now['dis'], $now[0]['X']), 
 										"cost" => ($now['cost'] + 1)
 									  ));
 		}
@@ -50,6 +63,7 @@ function ucs_helper($maze, $pqueue){
 			array_push($pqueue, array(	$maze[$now[0]['Y']][$now[0]['X']-1],
 										$now[0]['X'],
 										$now[0]['Y'],
+										"dis" => ucsH($now['dis'], $now[0]['X'] - 1), 
 										"cost" => ($now['cost'] + 1)
 									  ));
 		}
@@ -58,11 +72,11 @@ function ucs_helper($maze, $pqueue){
 			array_push($pqueue, array(	$maze[$now[0]['Y']-1][$now[0]['X']],
 										$now[0]['X'],
 										$now[0]['Y'],
+										"dis" => ucsH($now['dis'], $now[0]['X']), 
 										"cost" => ($now['cost'] + 1)
 									  ));
 		}
-		
-		return ucs_helper($maze, $pqueue);
+		return ucs_helper($maze, $pqueue, $counter+1, $frontier, $depth);
 	}
 }
 
